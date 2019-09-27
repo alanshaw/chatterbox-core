0d4f5f800684ba2670d5821749b596305c6ddb25
Alan Shaw <alan.shaw@protocol.ai>
2019-09-27 21:02:26 +0100

    test: add broadcast message test

--- src/messages/broadcast-message.js
+++ src/messages/broadcast-message.js
@@ -1,7 +1,9 @@
 const { Buffer } = require('buffer')
+const Validate = require('./validate')
 
 module.exports = ({ ipfs, addMessage, broadcastTopic }) => {
   return async text => {
+    Validate.text(text)
     const { id } = await ipfs.id()
     await addMessage(id, text)
     const data = Buffer.from(JSON.stringify({ text }))
