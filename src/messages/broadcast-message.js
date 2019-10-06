00e9e1e5ff818d2d8dcf04e04c1e95b23880f7ef
Alan Shaw <alan.shaw@protocol.ai>
2019-10-06 13:54:11 +0100

    fix: trim message before send

--- src/messages/broadcast-message.js
+++ src/messages/broadcast-message.js
@@ -5,6 +5,7 @@ const { PROTOCOL_VERSION } = require('./constants')
 module.exports = ({ ipfs, addMessage, broadcastTopic }) => {
   return async text => {
     Validate.text(text)
+    text = text.trim()
     const { id } = await ipfs.id()
     await addMessage(id, text)
     const data = Buffer.from(JSON.stringify({ version: PROTOCOL_VERSION, text }))
