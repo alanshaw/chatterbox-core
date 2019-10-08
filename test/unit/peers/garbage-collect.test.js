65dcd7871dad5b6281933b725c87b10e484b191a
Alan Shaw <alan.shaw@protocol.ai>
2019-10-08 20:49:46 +0100

    test: more gc tests

--- test/unit/peers/garbage-collect.test.js
+++ test/unit/peers/garbage-collect.test.js
@@ -19,17 +19,112 @@ const fakePeerInfo = (data = {}) => ({
   ...data
 })
 
+const overAnHourAgo = () => {
+  const oneHourAgo = Date.now() - (1000 * 60 * 60)
+  return oneHourAgo - Math.ceil(Math.random() * 100)
+}
+
 test('should collect peers last seen more than an hour ago', async t => {
   const repoDir = `/${Date.now()}`
   const peersPath = `${repoDir}/peers`
-
   const getPeerPath = peerId => `${peersPath}/${peerId}`
-  const oneHourAgo = Date.now() - (1000 * 60 * 60)
 
   const safePeers = [fakePeerInfo(), fakePeerInfo(), fakePeerInfo()]
   const garbagePeers = [
-    fakePeerInfo({ lastSeenAt: oneHourAgo - Math.ceil(Math.random() * 100) }),
-    fakePeerInfo({ lastSeenAt: oneHourAgo - Math.ceil(Math.random() * 100) })
+    fakePeerInfo({ lastSeenAt: overAnHourAgo() }),
+    fakePeerInfo({ lastSeenAt: overAnHourAgo() })
+  ]
+
+  const peersByPath = [...safePeers, ...garbagePeers].reduce((data, p) => {
+    data[getPeerPath(p.id)] = p
+    return data
+  }, {})
+
+  const getPeerInfo = peerId => peersByPath[getPeerPath(peerId)]
+
+  const ipfs = {
+    files: {
+      ls: () => Object.values(peersByPath).map(v => ({ name: v.id })),
+      rm: path => { delete peersByPath[path] }
+    }
+  }
+  const mutexManager = MutexManager()
+  const syndicate = {
+    publish: () => {}
+  }
+
+  const gc = GarbageCollect({
+    ipfs,
+    mutexManager,
+    peersPath,
+    getPeerPath,
+    getPeerInfo,
+    syndicate
+  })
+
+  await gc()
+
+  safePeers.forEach(p => t.true(Boolean(peersByPath[getPeerPath(p.id)])))
+  garbagePeers.forEach(p => t.false(Boolean(peersByPath[getPeerPath(p.id)])))
+})
+
+test('should collect peers never last seen', async t => {
+  const repoDir = `/${Date.now()}`
+  const peersPath = `${repoDir}/peers`
+  const getPeerPath = peerId => `${peersPath}/${peerId}`
+
+  const safePeers = [fakePeerInfo(), fakePeerInfo(), fakePeerInfo()]
+  const garbagePeers = [
+    fakePeerInfo({ lastSeenAt: null }),
+    fakePeerInfo({ lastSeenAt: null })
+  ]
+
+  const peersByPath = [...safePeers, ...garbagePeers].reduce((data, p) => {
+    data[getPeerPath(p.id)] = p
+    return data
+  }, {})
+
+  const getPeerInfo = peerId => peersByPath[getPeerPath(peerId)]
+
+  const ipfs = {
+    files: {
+      ls: () => Object.values(peersByPath).map(v => ({ name: v.id })),
+      rm: path => { delete peersByPath[path] }
+    }
+  }
+  const mutexManager = MutexManager()
+  const syndicate = {
+    publish: () => {}
+  }
+
+  const gc = GarbageCollect({
+    ipfs,
+    mutexManager,
+    peersPath,
+    getPeerPath,
+    getPeerInfo,
+    syndicate
+  })
+
+  await gc()
+
+  safePeers.forEach(p => t.true(Boolean(peersByPath[getPeerPath(p.id)])))
+  garbagePeers.forEach(p => t.false(Boolean(peersByPath[getPeerPath(p.id)])))
+})
+
+test('should not collect friends', async t => {
+  const repoDir = `/${Date.now()}`
+  const peersPath = `${repoDir}/peers`
+  const getPeerPath = peerId => `${peersPath}/${peerId}`
+
+  const safePeers = [
+    fakePeerInfo({ isFriend: true, lastSeenAt: overAnHourAgo() }),
+    fakePeerInfo({ isFriend: true, lastSeenAt: overAnHourAgo() }),
+    fakePeerInfo({ isFriend: true, lastSeenAt: overAnHourAgo() })
+  ]
+  const garbagePeers = [
+    fakePeerInfo({ lastSeenAt: overAnHourAgo() }),
+    fakePeerInfo({ lastSeenAt: overAnHourAgo() })
   ]
 
   const peersByPath = [...safePeers, ...garbagePeers].reduce((data, p) => {
